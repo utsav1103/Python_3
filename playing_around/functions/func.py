b9d0d758741174d06a22bc8bbfc4bfd655d678c9
utsav-baghela <utsavu858@gmail.com>
2025-09-06 18:54:51 +0530

    some in built functions

---
+++ playing_around/functions/func.py
@@ -0,0 +1,11 @@
+# . inbuilt
+name = "Mikasa Ackerman   "
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+print(name.strip()) #remove wide spaces
+print(name.rstrip())
+print(name.find("kasa"))#this returns index[]
+print(name.replace("ikasa","IKASA"))
+print("ker" in name)#it returns true or false
