47a760bf0b2002c42464c2ab268d2664795b3a7d
utsav-baghela <utsavu858@gmail.com>
2025-09-15 19:48:03 +0530

    math module

--- playing_around/functions/func.py
+++ playing_around/functions/func.py
@@ -1,15 +1,23 @@
-# . inbuilt
-name = "Mikasa Ackerman   "
+# # . inbuilt
+# name = "Mikasa Ackerman   "
 
-print(name.upper())
-print(name.lower())
-print(name.title())
-print(name.strip()) #remove wide spaces
-print(name.rstrip())
-print(name.find("kasa"))#this returns index[]
-print(name.replace("ikasa","IKASA"))
-print("ker" in name)#it returns true or false
+# print(name.upper())
+# print(name.lower())
+# print(name.title())
+# print(name.strip()) #remove wide spaces
+# print(name.rstrip())
+# print(name.find("kasa"))#this returns index[]
+# print(name.replace("ikasa","IKASA"))
+# print("ker" in name)#it returns true or false
 
 
-print(round(2.9))
-print(abs(-2.9))
+# print(round(2.9))
+# print(abs(-2.9))
+
+import math 
+
+print(math.ceil(2.2))
+print(math.copysign(2.2,3))
+print(math.factorial(2))
+print(math.floor(2.2))
+print(math.fabs(2.2))
