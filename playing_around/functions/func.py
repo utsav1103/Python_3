3008a7c8dae0d96f1fc2e249c04db50278e770cc
utsav-baghela <utsavu858@gmail.com>
2025-09-16 14:36:37 +0530

    data type and type fucntion in python

--- playing_around/functions/func.py
+++ playing_around/functions/func.py
@@ -16,8 +16,19 @@
 
 import math 
 
-print(math.ceil(2.2))
-print(math.copysign(2.2,3))
-print(math.factorial(2))
-print(math.floor(2.2))
-print(math.fabs(2.2))
+# print(math.ceil(2.2))
+# print(math.copysign(2.2,3))
+# print(math.factorial(2))
+# print(math.floor(2.2))
+# print(math.fabs(2.2))
+
+
+x = input("x: ")
+# print(type(x))
+y = int(x) + 1
+print(f"x: {x}, y: {y}")
+
+# int(x)
+# float(x)
+# bool(x)
+# str(x)
