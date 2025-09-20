98e97271c3c88730801b8897d8bb581c127e17d4
utsav-baghela <utsavu858@gmail.com>
2025-09-20 20:54:09 +0530

    if else

--- playing_around/operators/opp.py
+++ playing_around/operators/opp.py
@@ -1,9 +1,16 @@
-temperature = 16
-if temperature > 30:
-  print("It's warm")
-  print("Drink water")
-elif temperature > 35:
-  print("Nice")  
+# temperature = 16
+# if temperature > 30:
+#   print("It's warm")
+#   print("Drink water")
+# elif temperature > 35:
+#   print("Nice")  
+# else:
+#   print("It's Cold")  
+# print("Done")  
+
+
+age = 12
+if age >= 18:
+    print("Eligible")
 else:
-  print("It's Cold")  
-print("Done")  
+    print("Not Eligible")
