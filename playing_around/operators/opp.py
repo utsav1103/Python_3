b82ce6c5ee3fc3eab566a555adc70d9380b000bc
utsav-baghela <utsavu858@gmail.com>
2025-09-20 20:12:26 +0530

    conditions

--- playing_around/operators/opp.py
+++ playing_around/operators/opp.py
@@ -1,5 +1,9 @@
-temperature = 35
+temperature = 16
 if temperature > 30:
   print("It's warm")
   print("Drink water")
+elif temperature > 35:
+  print("Nice")  
+else:
+  print("It's Cold")  
 print("Done")  
