9c772edaa3b442c289cab69023377cefa0a9f5e9
utsav-baghela <utsavu858@gmail.com>
2025-09-28 18:29:33 +0530

    ,

--- playing_around/que.py
+++ playing_around/que.py
@@ -1,35 +1,23 @@
-def test_1(string=""):
+#
 
-    # initializing the substring
-    substring = ""
-    testList = []
-    initial = 0
-
-    for char in string:
-
-        for i in range(initial, len(string)):
-            substring += string[i]
-
-            # checking conditions
-            if substring.count(string[i]) > 1:
-                testList.append(substring[:-1])
-                initial += 1
-                substring = ""
-                break
-    maxi = ""
-
-    for word in testList:
-
-        if len(word) > len(maxi):
-            maxi = word
-
-    if len(maxi) < 3:
-        return "-1"
-    else:
-        return maxi
+def consecutive_range(l, k, s):
+  res = []
+  st, en = 0, 0
+  for i, n in enumerate(l):
+    if i < en:
+      continue
+    if n == k:
+      st = i
+      for j in range(i+1, len(l)):
+        if l[j] != k:
+            break
+      en = j-1
+      if (en-st >= s-1):
+        res.append((st, en))
+  return res
 
 
-# Driver code
-print(test_1("character"))
-print(test_1("standfan"))
-print(test_1("class"))
+consecutive_range(
+    l=[2, 6, 6, 6, 6, 5, 4, 6, 6, 8, 4, 6, 6, 6, 2, 6],
+    k=6,
+    s=3)
